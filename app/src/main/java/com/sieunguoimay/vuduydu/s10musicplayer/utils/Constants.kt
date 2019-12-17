35d9f4294747571f037d5272cebf0c9c8e034d9f
Sieunguoimay <vuduydu1997@gmail.com>
2019-12-17 11:52:49 +0530

    On Mobile data Network firebase not working bug

--- app/src/main/java/com/sieunguoimay/vuduydu/s10musicplayer/utils/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.sieunguoimay.vuduydu.s10musicplayer.utils
-
-object Constants {
-    val SHARED_PREFERENCE_NAME = "s10_shared_preference"
-    val SHUFFLE_STATE = "shuffle_state"
-    val PREVIOUS_TIMER = "previous_timer"
-    val DARK_MODE_ENABLED= "dark_mode"
-}
